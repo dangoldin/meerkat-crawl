831ef80d71b66b6a3b87188bb5d36b2afbce4a06
Dan Goldin <dangoldin@gmail.com>
2015-05-22 21:38:29 -0400

    a few more attempts

--- network.py
+++ network.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 
 import sys
+
 import networkx as nx
+from networkx.algorithms import find_cliques
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
@@ -23,5 +25,11 @@ if __name__ == '__main__':
     print G.number_of_nodes()
     print G.number_of_edges()
 
-    nx.draw_random(G)
-    plt.show()
+    # for x in find_cliques(G):
+    #     print x
+
+    # nx.draw_random(G)
+    # plt.show()
+
+    # nx.draw_random(G)
+    # plt.show()
